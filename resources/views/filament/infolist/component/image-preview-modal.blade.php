f04c8e310834bcb7f46874b76bdf12d51f931b55
darmawguna <komangagus642@gmail.com>
2025-09-05 18:32:59 +0800

    Remove image preview modal component from infolist

--- resources/views/filament/infolist/component/image-preview-modal.blade.php
+++
@@ -1,9 +0,0 @@
-@props([
-    'state',
-])
-
-<div class="flex justify-center p-2">
-    <img src="{{ \Illuminate\Support\Facades\Storage::url($state) }}"
-         alt="Pratinjau Lampiran"
-         style="max-width: 100%; height: auto; max-height: 75vh;" />
-</div>
